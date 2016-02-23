e4dc628e19a3070c88a3e63cc734c01074d644fe
nils-hamel <nils.hamel@bluewin.ch>
2016-02-23 09:31:46 +0100

    Adding initial module source

---
+++ src/eratosthene-include.h
@@ -0,0 +1,80 @@
+/*
+ *  liberatosthene - geodetic system
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2016 EPFL CDH DHLAB
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+    /*! \file   eratosthene-include.h
+     *  \author Nils Hamel <n.hamel@bluewin.ch>
+     *
+     *  Library includer
+     */
+
+/*
+    header - inclusion guard
+ */
+
+    # ifndef __LE_INCLUDE__
+    # define __LE_INCLUDE__
+
+/*
+    header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    extern "C" {
+    # endif
+
+/*
+    header - Includes
+ */
+
+/*
+    header - preprocessor definitions
+ */
+
+/*
+    header - preprocessor macros
+ */
+
+/*
+    header - type definition
+ */
+
+/*
+    header - structures
+ */
+
+/*
+    header - function prototypes
+ */
+
+/*
+    header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    }
+    # endif
+
+/*
+    header - inclusion guard
+ */
+
+    # endif
+
