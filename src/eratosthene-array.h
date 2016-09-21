1cd928983ad6da6d90275a393d66a9e9029380bd
nils-hamel <nils.hamel@bluewin.ch>
2016-09-21 15:00:41 +0200

    Doxygen documentation of array module

--- src/eratosthene-array.h
+++ src/eratosthene-array.h
@@ -139,27 +139,22 @@
     /*! \struct le_array_struct
      *  \brief array structure
      *
-     *  This structure simply holds the definition of an unidemensional array
-     *  storing four dimensionnal colorimetric points.
+     *  This structure holds the definition and content of a bytes array. These
+     *  arrays are commonly used to transmit data through sockets.
      *
-     *  The byte array memory pointed by the structure field is arranged in
-     *  successive lines containing seven values each : three spatial 
-     *  components, a time component and three colorimetric components.
+     *  Being a simple bytes succession, array are interpreted as more complexe
+     *  arrays of structure using memory mapper depending on the actual data
+     *  stored in the array.
      *
-     *  The spatial components are stored using \b le_real_t type when time is
-     *  coded using \b le_time_t type. The three colorimetric components are
-     *  stored using the \b le_data_t type.
-     *
-     *  In addition to the array size, giving the number of colorimetric points
-     *  stored in the array, the structure holds an asynchronous size used to
-     *  reduce the amount of memory dynamic reallocation.
+     *  The structures comes with methods allowing array size management and
+     *  socket i/o along with memory mapper structure initiated using macros.
      *
      *  \var le_array_struct::as_virt
-     *  Array asynchronous size, in number of points
+     *  Memory size of the bytes array, in bytes
      *  \var le_array_struct::ar_size
-     *  Array size, in number of points
+     *  Data size of the bytes array, in bytes
      *  \var le_array_struct::ar_byte
-     *  Pointer to array bytes.
+     *  Bytes array memory base pointer
      */
 
     typedef struct le_array_struct {
@@ -176,61 +171,148 @@
 
     /*! \brief constructor/destructor methods
      *
-     *  This function simply returns an array structure initialised with default
+     *  This function simply returns an array structure created using default
      *  values.
      *
-     *  \return Returns initialised structure
+     *  \return Returns created structure
      */
 
     le_array_t le_array_create( le_void_t );
 
     /*! \brief constructor/destructor methods
      *
-     *  This function uninitialise the provided array structure. It unallocates
-     *  memory when required and clears the structure field with default values.
+     *  This function delete the provided array structure. It frees array
+     *  memory when required and clears the structure fields.
      *
      *  \param le_array Array structure
-     *
-     *  \return Returns uninitialised structure
      */
 
     le_void_t le_array_delete( le_array_t * const le_array );
 
     /*! \brief accessor methods
      *
-     *  Returns the size, in elements count, of the array contained in the array
+     *  Returns the data size, in bytes, of the array contained in the provided
      *  structure.
      *
      *  \param le_array Array structure
      *
-     *  \return Array elements count
+     *  \return Array data size, in bytes
      */
 
     extern le_size_t le_array_get_size( le_array_t const * const le_array );
 
     /*! \brief accessor methods
      *
-     *  Returns the pointer to bytes of the array contained in the array
+     *  Returns the memory base pointer of the array contained in the provided
      *  structure.
      *
      *  \param le_array Array structure
      *
-     *  \return Returns pointer to array memory first byte
+     *  \return Returns array memory base pointer
      */
 
     extern le_byte_t * le_array_get_byte( le_array_t const * const le_array );
 
+    /*! \brief mutator methods
+     *
+     *  This function checks if an element of length \b le_length is addable to
+     *  the array memory allocation. When the array memory is not enough, it
+     *  reallocate the array memory allowing the addition of the element.
+     *
+     *  \param le_array  Array structure
+     *  \param le_length Length, in bytes, of added element
+     *
+     *  \return Return _LE_ERROR_SUCCESS on success, an error code otherwise
+     */
+
     le_enum_t le_array_set_memory( le_array_t * const le_array, le_size_t const le_length );
+
+    /*! \brief mutator methods
+     *
+     *  This function is used as a complexe record insertion method in the bytes
+     *  array hold by the provided structure.
+     *
+     *  It expects a position 3-vector, a time value and a data 3-vector as
+     *  parameters and pack them in a bytes sequence that is inserted at the end
+     *  of the provided array.
+     *
+     *  \param le_array Array structure
+     *  \param le_pose  Position 3-vector
+     *  \param le_time  Time value
+     *  \param le_data  Data 3-vector
+     */
 
     le_void_t le_array_set_pushsf( le_array_t * const le_array, le_real_t const * const le_pose, le_time_t const le_time, le_data_t const * const le_data );
 
+    /*! \brief mutator methods
+     *
+     *  This function is used as a complexe record insertion method in the bytes
+     *  array hold by the provided structure.
+     *
+     *  It expects a position 3-vector and a data 3-vector as parameters and
+     *  pack them in a bytes sequence that is inserted at the end of the
+     *  provided array.
+     *
+     *  \param le_array Array structure
+     *  \param le_pose  Position 3-vector
+     *  \param le_data  Data 3-vector
+     */
+
     le_void_t le_array_set_pushrf( le_array_t * const le_array, le_real_t const * const le_pose, le_data_t const * const le_data );
+
+    /*! \brief mutator methods
+     *
+     *  This function is used as a complexe record insertion method in the bytes
+     *  array hold by the provided structure.
+     *
+     *  It expects a time value as parameter and pack them in a bytes sequence
+     *  that is inserted at the end of the provided array.
+     *
+     *  \param le_array Array structure
+     *  \param le_time  Time value
+     */
 
     le_void_t le_array_set_pushtf( le_array_t * const le_array, le_time_t const le_time );
 
+    /*! \brief mutator methods
+     *
+     *  This function is used as a complexe record insertion method in the bytes
+     *  array hold by the provided structure.
+     *
+     *  It expects a configuration size and a configuration time as parameters
+     *  and pack them in a bytes sequence that is inserted at the end of the
+        provided array.
+     *
+     *  \param le_array Array structure
+     *  \param le_size  Configuration size
+     *  \param le_time  Configuration time
+     */
+
     le_void_t le_array_set_pushcf( le_array_t * const le_array, le_size_t const le_size, le_time_t const le_time );
 
+    /*! \brief i/o methods
+     *
+     *  This function writes the data content from the provided array on the
+     *  provided opened socket.
+     *
+     *  \param le_array  Array structure
+     *  \param le_socket Opened socket
+     *
+     *  \return Return _LE_ERROR_SUCCESS on success, an error code otherwise
+     */
+
     le_enum_t le_array_io_write( le_array_t const * const le_array, le_sock_t const le_socket );
+
+    /*! \brief i/o methods
+     *
+     *  This function reads the data content of the provided array from the
+     *  provided opened socket.
+     *
+     *  It reads bytes blocks as their are coming until connection is closed
+     *  or socket become silent.
+     *
+     *  \return Returns _LE_ERROR_SUCCESS on success, an error code otherwise
+     */
 
     le_enum_t le_array_io_read( le_array_t * const le_array, le_sock_t const le_socket );
 
