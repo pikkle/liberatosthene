4d3df37b3575d996efcec717be11f5e851dfeed1
nils-hamel <nils.hamel@bluewin.ch>
2017-08-15 13:24:02 +0200

    Duplicated include removal - added inet tcp header

--- src/eratosthene.h
+++ src/eratosthene.h
@@ -98,7 +98,7 @@
     # include <sys/types.h>
     # include <sys/socket.h>
     # include <netinet/in.h>
-    # include <netinet/in.h>
+    # include <netinet/tcp.h>
     # include <arpa/inet.h>
     # include <netdb.h>
     # include <pthread.h>
