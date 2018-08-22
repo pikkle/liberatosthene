9c553790b0fec9f8599a974e1ac223a74911f01c
nils-hamel <nils.hamel@bluewin.ch>
2018-08-22 16:00:27 +0200

    Restoring revoked documentation in door and switch modules

--- src/eratosthene-door.h
+++ src/eratosthene-door.h
@@ -89,7 +89,44 @@
     header - structures
  */
 
-    /* *** */
+
+    /*! \struct le_unit_struct
+     *  \brief unit structure ( revoked )
+     *
+     *  This structure holds the required information to manage and access a
+     *  specific storage structure of a temporal unit.
+     *
+     *  The two first fields are used to store a copy of the server storage
+     *  structure root path and the spatial configuration value that are used
+     *  for unit manipulation.
+     *
+     *  The next field is used to store the time value associated to the unit.
+     *  The value stored in this field is according to the equivalence classes
+     *  point of view :
+     *
+     *      time_unit = time_utc / server_temporal_parameter
+     *
+     *  The unit time is then called "reduced".
+     *
+     *  The next field of the structure holds the stack of stream opened
+     *  toward the file of the unit storage structure. Each scale file is then
+     *  described by its corresponding stream in the pile.
+     *
+     *  A last field is used by the structure creation methods to specified the
+     *  creation status of the structure. This allows to creation methods to
+     *  return the structure instead of an error code.
+     *
+     *  \var le_unit_struct::un_root
+     *  Server storage structure path
+     *  \var le_unit_struct::un_scfg
+     *  Server spatial configuration value : number of scale
+     *  \var le_unit_struct::un_time
+     *  Unit time in reduced form
+     *  \var le_unit_struct::un_pile
+     *  Unit stream pile
+     *  \var le_unit_struct::_status
+     *  Standard status field
+     */
 
     typedef struct le_door_struct {
 
@@ -115,11 +152,43 @@
     header - function prototypes
  */
 
-    /* *** */
+    /*! \brief constructor/destructor methods ( revoked )
+     *
+     *  This function creates a unit structure according to the provided path
+     *  and to the provided mode.
+     *
+     *  Two modes are available : \b LE_UNIT_READ and \b LE_UNIT_WRITE. The
+     *  first mode triggers the creation of the unit structure according to an
+     *  existing storage structure. It assign the provided time value, in its
+     *  reduced form, before to create the stream descriptor to the scale files.
+     *
+     *  The second mode creates the storage structure directory if necessary and
+     *  creates the scale files while creating the streams to them.
+     *
+     *  If any stream is not accessible or if the directory creation fails, the
+     *  function returns the structure as it is.
+     *
+     *  This function returning the created structure, the status is stored in
+     *  the structure itself using the reserved \b _status field.
+     *
+     *  \param le_root   Server storage structure path
+     *  \param le_scfg   Server spatial configuration value
+     *  \param le_time   Unit time value, in reduced form
+     *  \param le_mode   Access mode value
+     *
+     *  \return Returns the created unit
+     */
 
     le_door_t le_door_create( le_char_t const * const le_root, le_size_t const le_scfg, le_time_t const le_tcfg, le_time_t const le_time, le_enum_t const le_mode );
 
-    /* *** */
+    /*! \brief constructor/destructor methods ( revoked )
+     *
+     *  This function deletes the provided unit structure. It parses the scales
+     *  of the unit and deletes each scale opened stream. It finally resets the
+     *  unit structure fields.
+     *
+     *  \param le_unit Unit structure
+     */
 
     le_void_t le_door_delete( le_door_t * const le_door );
 
@@ -167,7 +236,27 @@
 
     le_enum_t le_door_io_each_inject_filter( le_door_t const * const le_door, le_array_t const * const le_array );
 
-    /* *** */
+    /*! \brief i/o methods ( revoked )
+     *
+     *  This function writes the content of the incoming array in the server
+     *  storage structure.
+     *
+     *  The provided array is expect to have the UF3 format, that is a series
+     *  of records composed of a coordinates 3-vector and a colour 3-vector.
+     *
+     *  For each array record, the function initiates an address structure using
+     *  the record coordinates to compute the spatial index. The index is then
+     *  used to drive the progression of the record injection through the scale
+     *  files.
+     *
+     *  For a given scale, the class the record belong to is searched. If the
+     *  class is found, the element is added to the class. Otherwise, a new
+     *  class is created using the record to initialise it. The parent class
+     *  offsets array is updated to take into account the new created class.
+     *
+     *  \param le_unit  Unit structure
+     *  \param le_array Array structure
+     */
 
     le_enum_t le_door_io_mono_inject( le_door_t const * const le_door );
 
@@ -183,15 +272,66 @@
 
     le_enum_t le_door_io_each_inject_clean( le_door_t const * const le_door );
 
-    /* *** */
+    /*! \brief mutator methods ( revoked )
+     *
+     *  This function allows to optimise the storage structure of the provided
+     *  unit structure. The function rewrites the storage structure of the unit
+     *  to allow faster read operations.
+     *
+     *  The function starts by creating "dual" files for each scales of the unit
+     *  in which the content of the current file is re-written in an optimised
+     *  fashion. At the end of the process, the "dual" files are move to over-
+     *  write the unit previous files.
+     *
+     *  This operation being heavy in terms of read and write operation, it can
+     *  be long on large unit storage structure.
+     *
+     *  \param le_unit Unit structure
+     */
 
     le_void_t le_door_io_mono_optimise( le_door_t * const le_door );
 
-    /* *** */
+    /*! \brief i/o methods ( revoked )
+     *
+     *  This recursive function is the function used by the storage optimisation
+     *  procedure implemented in \b le_unit_set_optimise(). This function is
+     *  used to read the storage structure as the tree it defines. In the same
+     *  time, the read classes of the different scales are rewritten in their
+     *  "tree-reading" order in the provided "dual" files. The recursive process
+     *  ends as the tree is entirely read, and so, written in the "dual" files.
+     *
+     *  The "dual" stream array has to have, at least, a size equal to the
+     *  server space parameter. In addition, it as to contain already created
+     *  file descriptor toward the optimisation "dual" files.
+     * 
+     *  In the same way, the class writing offsets tracker array (\b le_head)
+     *  has the same constraint on its size. In addition, its content has to be
+     *  zero for each of it used elements.
+     *
+     *  \param le_unit   Unit structure
+     *  \param le_dual   Dual stream array
+     *  \param le_offset Class offset
+     *  \param le_head   Class writing offsets tracker
+     *  \param le_scale  Scale number
+     */
 
     le_void_t le_door_io_mono_optimise_rewrite( le_door_t * const le_door, le_file_t const * const le_dual, le_size_t const le_offset, le_size_t * const le_head, le_size_t const le_scale );
 
-    /* *** */
+    /*! \brief i/o methods ( revoked )
+     *
+     *  This function searches the offset of the spatial class pointed by the
+     *  spatial index provided through the address structure in the provided
+     *  unit storage structure.
+     *
+     *  The spatial index digits are used to drive the progression through
+     *  the structure scales. This function is mainly used to detect if the
+     *  class pointed by the address exists or not.
+     *
+     *  \param le_unit Unit structure
+     *  \param le_addr Address structure
+     *
+     *  \return Returns the class offset on success, _LE_OFFS_NULL otherwise
+     */
 
     le_enum_t le_door_io_mono_detect( le_door_t * const le_door, le_address_t const * const le_addr );
 
@@ -199,7 +339,28 @@
 
     le_enum_t le_door_io_poly_detect( le_door_t * const le_door, le_address_t const * const le_addr );
 
-    /* *** */
+    /*! \brief i/o methods ( revoked )
+     *
+     *  This function is used to gather spatial classes positions and colours
+     *  information to build a array using the UF3 format.
+     *
+     *  This function assume that the offset of the spatial class pointed by the
+     *  address index is known (\b le_tree_io_offset()). Starting from the
+     *  offsets array of this class, it starts to gather the position and
+     *  colours by enumerating the class daughters and sub-daughters through a
+     *  recursive process. It detect the gathering scale using the main class
+     *  scale and the address additional depth (span).
+     *
+     *  If the provided array is not passed empty, its previous content is left
+     *  unchanged, the function pushing the elements at the end of it.
+     *
+     *  \param le_unit   Unit structure
+     *  \param le_addr   Address structure
+     *  \param le_offset Class storage offset
+     *  \param le_parse  Class storage scale
+     *  \param le_span   Query additional depth
+     *  \param le_array  Data array filled by the function
+     */
 
     le_void_t le_door_io_mono_gather( le_door_t * const le_door, le_address_t * const le_addr, le_size_t const le_parse, le_size_t const le_span, le_array_t * const le_array );
 
@@ -207,7 +368,33 @@
 
     le_void_t le_door_io_poly_gather( le_door_t * const le_door, le_address_t * const le_addr, le_size_t const le_parse, le_size_t const le_span, le_array_t * const le_array );
 
-    /* *** */
+    /*! \brief i/o methods ( revoked )
+     *
+     *  This function implements a parallel version of \b le_tree_io_gather()
+     *  function. It performs the same operations simply considering two times
+     *  values.
+     *
+     *  As two parallel gathering processes take place in this function, it
+     *  allows to consider the times comparison mode provided by the address
+     *  structure. As elements for the two times are gathered at the same time,
+     *  the function is able to easily implements the logical operators. The
+     *  array is then filled with the results of the application of the logical
+     *  operators.
+     *
+     *  If the provided array is not passed empty, its previous content is left
+     *  unchanged, the function pushing the elements at the end of it.
+     *  of it.
+     *
+     *  \param le_unia    Unit structure - ass. to time 1
+     *  \param le_unib    Unit structure - ass. to time 2
+     *  \param le_addr    Address structure
+     *  \param le_mode    Address mode
+     *  \param le_offseta Class storage offset - ass. to time 1
+     *  \param le_offsetb Class storage offset - ass. to time 2
+     *  \param le_parse   Class storage scale
+     *  \param le_span    Query additional depth
+     *  \param le_array   Data array filled by the function
+     */
 
     le_void_t le_door_io_mono_parallel( le_door_t * const le_pdoor, le_door_t * const le_sdoor, le_address_t * const le_addr, le_byte_t const le_mode, le_size_t const le_parse, le_size_t const le_span, le_array_t * const le_array );
 
--- src/eratosthene-switch.h
+++ src/eratosthene-switch.h
@@ -74,7 +74,45 @@
     header - structures
  */
 
-    /* *** */
+    /*! \struct le_tree_struct
+     *  \brief tree structure ( revoked )
+     *
+     *  This structure holds the required element to access the server storage
+     *  structure. Usually, each client thread of the server holds its own
+     *  instance of this structure that it uses for data access and export.
+     *
+     *  The three first field are copies of the server storage structure path
+     *  and space-time configuration parameters. The next field is a statically
+     *  initialised value that indicates the tree associated function to discard
+     *  time units due to their temporal distance of the point of view. This
+     *  allow to avoid mixing too much model that belong to too different times.
+     *
+     *  The next fields are the size of the unit stack and the stack itself. The
+     *  stack is an array of unit structures that each contain the required data
+     *  to access one time unit. It follows that any data injected in the
+     *  storage structure is contained in one of those unit, distinguished
+     *  through their time. It follows that any access starts by searching the
+     *  unit associated to a time value.
+     *
+     *  A last field is used by the structure creation methods to specified the
+     *  creation status of the structure. This allows to creation methods to
+     *  return the structure instead of an error code.
+     *  
+     *  \var le_tree_struct::tr_root
+     *  Server storage structure path
+     *  \var le_tree_struct::tr_scfg
+     *  Server spatial configuration value : number of scale
+     *  \var le_tree_struct::tr_tcfg
+     *  Server temporal configuration value : size of temporal classes
+     *  \var le_tree_struct::tr_comb
+     *  Server temporal comb size
+     *  \var le_tree_struct::tr_size
+     *  Tree unit stack size (number of units)
+     *  \var le_tree_struct::tr_unit
+     *  Tree unit stack
+     *  \var le_tree_struct::_status
+     *  Standard status field
+     */
 
     typedef struct le_switch_struct {
 
@@ -92,19 +130,93 @@
     header - function prototypes
  */
 
-    /* *** */
+    /*! \brief constructor/destructor methods ( revoked )
+     *
+     *  This function creates and initialise a tree structure according to the
+     *  provided storage structure path.
+     *
+     *  The function starts by enumerating the temporal units directory and
+     *  create for each of them a corresponding unit structure using their
+     *  specific creation method.
+     *
+     *  Each unit structure is inserted in the tree stack in growing order from
+     *  their respective time values. The time value of each unit is read from
+     *  their directory name. As the stack is entirely filled, the function
+     *  returns the created structure.
+     *
+     *  This function returning the created structure, the status is stored in
+     *  the structure itself using the reserved \b _status field.
+     *
+     *  \param le_root Server storage structure path
+     *  \param le_scfg Server spatial configuration value
+     *  \param le_tcfg Server temporal configuration value
+     *
+     *  \return Returns the created structure
+     */
 
     le_switch_t le_switch_create( le_char_t * const le_path, le_size_t const le_scfg, le_time_t const le_tcfg );
 
-    /* *** */
+    /*! \brief constructor/destructor methods ( revoked )
+     *
+     *  This function deletes the provided tree structure. It starts by parsing
+     *  the tree unit structures stack to delete them using their specific
+     *  method. The function then release the stack memory and clears the tree
+     *  structure fields.
+     *
+     *  \param le_tree Tree structure
+     */
 
     le_void_t le_switch_delete( le_switch_t * const le_switch );
 
-    /* *** */
+    /*! \brief accessor methods ( revoked )
+     * 
+     *  This function search in the provided tree structure stack the unit
+     *  structure that matches exactly the provided time value.
+     *
+     *  If such a unit is found in the tree stack, it is returned through its
+     *  pointer. In case no unit can be found in the stack, the behaviour of the
+     *  function depends on the provided mode value.
+     *
+     *  If the mode \b LE_UNIT_READ is provided and no unit is found, a NULL
+     *  pointer is returned. If no unit is found and the \b LE_UNIT_WRITE is
+     *  provided as mode value, the function ask for the creation of the unit
+     *  storage allocation. The created unit structure is inserted in the tree
+     *  stack and its pointer is returned.
+     *
+     *  \param le_tree Tree structure
+     *  \param le_time Time value
+     *  \param le_mode Access mode value
+     *
+     *  \return Returns the unit structure on success, NULL otherwise
+     */
 
     le_door_t * le_switch_get_inject( le_switch_t * const le_switch, le_time_t const le_time, le_enum_t const le_mode );
 
-    /* *** */
+    /*! \brief accessor methods ( revoked )
+     *
+     *  This function search in the provided tree structure stack the nearest
+     *  unit structure and its corresponding offset in which the class provided
+     *  through the address structure is found and not empty. The proximity is
+     *  understood from a temporal point of view.
+     *
+     *  The function starts by reading the provided address time using its index
+     *  and start to search which unit structure is able to give access to the
+     *  class pointed by the address structure digit. It searches, in both
+     *  direction, through time for the nearest unit containing the non-empty
+     *  class.
+     *
+     *  As a unit structure is found, it is returned by the function along with
+     *  the offset of the class in the storage structure described by the unit
+     *  structure. The offset is also returned in order to allow further
+     *  optimisation of subsequent gathering processes.
+     *
+     *  \param le_tree   Tree structure
+     *  \param le_addr   Address structure
+     *  \param le_addrt  Offset of time - zero based
+     *  \param le_offset Returned offset of the found class
+     *
+     *  \return Returns the unit structure on success, NULL otherwise
+     */
 
     le_door_t * le_switch_get_query( le_switch_t const * const le_switch, le_time_t const le_time, le_address_t const * const le_addr );
 
@@ -116,7 +228,31 @@
 
     le_enum_t le_switch_set_build( le_switch_t * const le_switch );
 
-    /* *** */
+    /*! \brief mutator methods ( revoked )
+     *
+     *  This function adds the unit corresponding to the provided time to the
+     *  provided tree according to the specified mode value.
+     *
+     *  The function searches if the time value points to an existing storage
+     *  structure. As the structure is found, the function creates a new unit in
+     *  the tree stack and initialises it for the found storage structure.
+     *
+     *  If no unit storage structure is not found, the behaviour of the function
+     *  depends on the access mode : \b LE_UNIT_READ indicates the function to
+     *  fail. Providing \b LE_UNIT_WRITE tells the function to create and
+     *  initialise a new storage structure corresponding to the provided time
+     *  value.
+     *
+     *  If a new unit is pushed in the provided tree structure stack, it is
+     *  always inserted in such a way the stack gives always unit in growing
+     *  order accroding to their time value.
+     *
+     *  \param le_tree Tree structure
+     *  \param le_time Time value
+     *  \param le_mode Access mode value
+     *
+     *  \return Returns unit structure on success, NULL otherwise
+     */
 
     le_door_t * le_switch_set_push( le_switch_t * const le_switch, le_time_t const le_time, le_enum_t const le_mode );
 
