e889ce08390d048fb7dfa58ed5172e9d8f7a6f21
nils-hamel <nils.hamel@bluewin.ch>
2018-11-06 11:04:39 +0100

    Revision of the README and doxygen documentation main page

--- src/eratosthene.h
+++ src/eratosthene.h
@@ -29,59 +29,63 @@
      *  \section overview Overview
      *
      *  The _liberatosthene_ library implements an earth scale 4D tile server
-     *  through a dedicated space-time index formalism. Using the theoretical
-     *  framework of the index, it implements a simple and reliable 4D data
-     *  storage and access. This allows servers developed using this library to
-     *  perform data injection and query with high efficiency over the network
-     *  to provide 4D geographic services and information systems.
-     *
-     *  From a theoretical point of view, any type of point-based data, as
-     *  referenced in space and time, can be considered through the index
-     *  formalism. Currently, colours are considered as data. This allows
-     *  servers built on this library to broadcast large amount of colorimetric
-     *  points over the network, toward large and multiscale 4D geographic
-     *  services information systems at earth scale.
+     *  through a dedicated multi-scale space-time indexation formalism. Using
+     *  the theoretical framework of the index, it implements a simple and
+     *  reliable 4D geographical data storage and access system. This allows
+     *  software developed using this library to perform large-scale data
+     *  injection and browsing with high efficiency over the network to provide
+     *  remote 4D geographic services and information systems. Initially
+     *  designed for point-based data, the indexation formalism and the library
+     *  implementation allow now to manage polygonal representations along with
+     *  large scale point-based models to create a rich 4D model of the earth.
      *
      *  \section project Eratosthene Project
      *
-     *  The Eratosthene Project was initially imagined to answer the question of
-     *  very large point-based models of cities and environments. Indeed,
-     *  mapping a whole city at the level of the centimetre induces very large
+     *  The Eratosthene Project was initially designed to address the challenge
+     *  of very large point-based models of cities and environments. Indeed,
+     *  mapping a whole city at the level of the centimeter induces very large
      *  and heavy models that are difficult to manipulate, explore and exploit.
      *  Moreover, considering the time dimension implies a quick increase of the
      *  size of the models, making them impossible to handle as a single and
      *  monolithic object.
      *
-     *  The Eratosthene Project is designed to answer this question by
-     *  implementing an space-time indexation formalism able to handle, store
-     *  and access very large 4D models of cities and environments. The
-     *  indexation formalism is used to implement a server able to manage, store
-     *  and broadcast very large 4D models distributed all over the earth
-     *  surface and covering large period of time. In addition to the server,
-     *  the Eratosthene Project also implements a graphical client able to
-     *  communicate with the server and to offer a convenient way of browsing
-     *  the models in both space and time.
-
+     *  The Eratosthene Project is designed to provide a solution by considering
+     *  a space-time indexation formalism. This indexation formalism is used to
+     *  implement a server able to manage, store and broadcast over the network
+     *  very large 4D models distributed all over the earth surface and covering
+     *  large periods of time. In addition to the server, the Eratosthene
+     *  Project also implements a graphical client able to communicate with the
+     *  remote servers to offer a convenient way to browse the models in both
+     *  space and time.
+     *
      *  The space-time indexation formalism is designed to efficiently manage
-     *  the storage of the incoming 4D models and to offer and simple and
-     *  efficient ways of addressing queries to the server allowing models
+     *  the storage of the incoming 4D models and to offer a simple and
+     *  efficient way of addressing queries to the server to allow models
      *  broadcasting. The space-time indexation also allows to perform queries
-     *  to the server in a intuitive way giving the possibility to both address
-     *  the questions of the earth structures level of detail and time
+     *  to the server in an intuitive way giving the possibility to both address
+     *  the challenge of the earth structures level of details and time
      *  management. This allows to explore the entire earth surface in terms of
      *  4D structures from large mountain chains in 2000 to very small details
      *  of cities in 1850.
      *
      *  The subsequent indexation formalism used to drive the storage and access
      *  of the data also naturally brings a standardised discretisation of space
-     *  and time. Models injected in the information system are then
-     *  standardised from both spatial and temporal point of view. This allows
-     *  the implementation of binary and logical operators when considering a
-     *  model at two different times. The operators allows to deduce a third
-     *  model providing a differential point of view of the two models separated
-     *  in time. The evolution of cities and environments can be enhanced and
-     *  analysed in an efficient way, opening the way to more advanced
-     *  geographic services.
+     *  and time. Point-based models injected in the Eratosthene information
+     *  system are then standardised from both spatial and temporal point of
+     *  views. This allows the implementation of binary and logical operators
+     *  when considering a model at two different times. The operators allow to
+     *  deduce a third model on the fly providing a differential point of view
+     *  of the two models separated in time. The evolution of cities and
+     *  environments can be enhanced and analysed in an efficient way, opening
+     *  the way to more advanced geographic services.
+     *
+     *  The ability of indexation formalism to consider polygonal models in
+     *  addition to large scale point-based models allows to enhance the
+     *  possibilities in terms of 4D geographic services. More than the
+     *  possibility to automatically manage the polygonal models level of
+     *  details and large scale display, the indexation formalism also allows
+     *  compositions on the fly of mixed models allowing to compare different
+     *  type of models, from lidar point-based models to land registers.
      *
      *  \section license Copyright and License
      *
